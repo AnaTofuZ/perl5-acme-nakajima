7cb9dcfc73697a84ea40c5ea0503b91a235c57f0
AnaTofuZ <e155730@ie.u-ryukyu.ac.jp>
2017-04-03 23:45:32 +0900

    add repository

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::Nakajima - It's new $module
+
+# SYNOPSIS
+
+    use Acme::Nakajima;
+
+# DESCRIPTION
+
+Acme::Nakajima is ...
+
+# LICENSE
+
+Copyright (C) AnaTofuZ.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+AnaTofuZ <e155730@ie.u-ryukyu.ac.jp>
---
+++ lib/Acme/Nakajima.pm
@@ -0,0 +1,40 @@
+package Acme::Nakajima;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::Nakajima - Isono let's play baseball!
+
+=head1 SYNOPSIS
+
+    use Acme::Nakajima;
+
+=head1 DESCRIPTION
+
+Acme::Nakajima is a charcter from Sazae-san.
+Sazae-san is Japanese famous animation.
+
+=head1 LICENSE
+
+Copyright (C) AnaTofuZ.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+AnaTofuZ E<lt>e155730@ie.u-ryukyu.ac.jpE<gt>
+
+=cut
+
